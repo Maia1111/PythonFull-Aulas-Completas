56f2b132b671e6dbd7204d14e4430ff69ee9c4fd
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-03-14 10:07:22 -0400

    Capturando parametros na url

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,14 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def cadastro(requests):
+def cadastro(requests): 
+   nome = requests.GET.get('nome')
+   sobrenome = requests.GET.get('sobrenome')
+   idade = requests.GET.get('idade')
+   print(nome, sobrenome, idade) 
+   return render(requests, 'index.html', {'nome': nome, 'sobrenome': sobrenome, 'idade': idade})
 
-
-  pessoa = [{'nome':'Caio', 'idade': 13, 'profissão': 'Programador'},
-            {'nome':'Marcos', 'idade': 22, 'profissão': 'Analista de sistema'},
-            {'nome':'Paulo', 'idade': 14, 'profissão': 'Jogador'},
-            {'nome':'Fabricio', 'idade': 15, 'profissão': 'Policial'},
-            {'nome':'Jose', 'idade': 40, 'profissão': 'Administrador'},
-                          
-            ]
-  return render(requests, 'index.html', {'pessoa': pessoa})
