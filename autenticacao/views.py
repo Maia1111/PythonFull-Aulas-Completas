fdf19c31fb7178381841df0b6362a4349fa09516
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-03-14 19:48:29 -0400

    Aula Foreign 3 terminada

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from.models import Pessoa
+from.models import *
 
 def cadastro(request): 
     if request.method =="GET":
@@ -12,12 +12,27 @@ def cadastro(request):
          nome = request.POST.get('nome')
          email = request.POST.get('email')
          senha = request.POST.get('senha')
+         cargo = Cargos.objects.filter(id = 1).first()
          
 
-         pessoa = Pessoa(nome=nome, email=email, senha=senha)
+         pessoa = Pessoa(nome=nome, email=email, senha=senha, cargo=cargo)
          pessoa.save()
          return HttpResponse('Cadatrado com sucesso')
-        
+    
+
+   
+def listar(request):
+   if len(request.GET) != 0:
+       nome = request.GET.get('nome')
+       email = request.GET.get('email')
+       senha = request.GET.get('senha')
+       cargo = Cargos.objects.filter(id = 1).first()
+       pessoa = Pessoa(nome=nome, email=email, senha=senha, cargo=cargo)
+       pessoa.save()
+       print(pessoa)
+
+   pessoas = Pessoa.objects.all()
+   return render(request, 'listar.html', {'pessoas': pessoas})
 
 
   
