c1b75d0347438d3ae0dd6cc9cd6db3383814cc93
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-03-17 15:07:14 -0400

    Configurado no arquivo settings.py  Tome Zone como America/Cuiabá

--- aula_django/settings.py
+++ aula_django/settings.py
@@ -99,9 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Cuiaba'
 
 USE_I18N = True
 
