40a7f55a620d263c70ec99db94834ca2ff4517f3
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-03-17 18:05:53 -0400

    Terminado aula inserir imagem em cadastro do admin do django

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from autenticacao.models import Pessoa, Cargos
+from autenticacao.models import Pessoa, Cargos, Pedido
 
-admin.site.register(Pessoa)
+
+
+class PedidoInline(admin.TabularInline):
+    list_display  = ('nome', 'quantidade', 'descricao')
+    model = Pedido
+    extra = 3
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    inlines = [PedidoInline]
+    list_display = ('get_foto', 'nome', 'email', 'senha', 'nome_completo',)
+    search_fields = ('nome',)
+    list_filter = ('cargo',)
+
+
+admin.site.register(Pedido)
 admin.site.register(Cargos)
+
 
 
--- autenticacao/models.py
+++ autenticacao/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.safestring import mark_safe
 
 class Cargos(models.Model):
     nome = models.CharField(max_length=20)
@@ -9,11 +11,31 @@ class Cargos(models.Model):
     
 
 class Pessoa(models.Model):
+    foto = models.ImageField(upload_to='foto')
     nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
     email = models.EmailField()
     senha = models.CharField(max_length=100)
     cargo = models.ManyToManyField(Cargos)
 
     def __str__(self):
         return self.nome
+    
+    def nome_completo(self):
+        return f"{self.nome} {self.sobrenome}"
+    
+    
+    
+    def get_foto(self):
+        return mark_safe(f'<img style="width: 60px;" src="{self.foto.url}">')
+    
+class Pedido(models.Model):   
+    nome = models.CharField(max_length=100)    
+    quantidade = models.IntegerField()
+    descricacao = models.TextField()
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.DO_NOTHING)
+
+
+
+
 
